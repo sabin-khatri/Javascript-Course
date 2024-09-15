31e750041c714e8250c0625baef7319bf64054dc
Sabin Khatri <khatrisabin611@gmail.com>
2024-09-15 08:22:56 +0545

    javascript course

---
+++ ep-03-javascript-variables/script.js
@@ -0,0 +1,15 @@
+console.log(firstName)
+
+var firstName = 'Sabin'
+let lastName = 'Khatri'
+let age = 15
+firstName = 100
+var a
+
+const hoursInDay = 24
+
+
+let userIntro = 
+    'Hi, my name is ' 
+    + firstName + ' ' 
+    + lastName + '. I am ' + age + ' ' + 'years old.'
