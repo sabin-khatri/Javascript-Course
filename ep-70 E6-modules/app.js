a94c336582c96092a8c4120fa0ff1f09f2ba607c
sabin khatri <khatrisabin611@gmail.com>
2024-11-12 07:49:41 +0545

    es6 modules

---
+++ ep-70 E6-modules/app.js
@@ -0,0 +1,5 @@
+// app.js
+import { add, subtract } from './math.js';
+
+console.log("Addition of 3 and 5:", add(3, 5));       // Output: 8
+console.log("Subtraction of 10 and 4:", subtract(10, 4)); // Output: 6
